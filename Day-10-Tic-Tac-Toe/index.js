fb94c2d07a64b943e844645e927a53783cca09e8
shiv12a10 <96536840+shiv12a10@users.noreply.github.com>
2023-01-18 22:25:23 +0530

    Day-10
    Done almost part of Tic-Tac-Toe  Game , and will be finishing rest of the part next day.

---
+++ Day-10-Tic-Tac-Toe/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector(',announcer');
+
+
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+    
+})
